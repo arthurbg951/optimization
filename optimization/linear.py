8480dd479b52013bcb7580ecfa8947a14b519132
Arthur Brito <arthurbg951@hotmail.com>
2024-09-19 20:29:32 -0300

    Atualizando código para notação das referencias

--- optimization/linear.py
+++ optimization/linear.py
@@ -11,90 +11,82 @@ def passo_constante(
     p0: np.ndarray,
     alfa: float,
     n: np.ndarray,
-    func: Callable[[float, float], float],
+    func: Callable[[np.ndarray], float],
     n_max_step: int = 1000,
     verbose: bool = False,
-    monitor: list = None,
+    monitor: list[float] = None,
 ) -> tuple[np.ndarray, np.ndarray]:
     if verbose:
         print(y("Inicializando Método do Passo Constante"))
-    if monitor is not None:
-        monitor.append(p0)
 
     # Realiza 1 passo para verificar se é necessário inverter a direção
-    check = make_step(p0, alfa, n)
-    f_check = func(check[0], check[1])
-    if f_check > func(p0[0], p0[1]):
-        n = -n
+    p_check = make_step(p0, 1e-6, n)
+    f_check = func(p_check)
+    if f_check > func(p0):
+        alfa = -alfa
 
+    p_buff = p0
     for i in range(n_max_step):
-        p1 = make_step(p0, alfa, n)
-        f0 = func(p0[0], p0[1])
-        f1 = func(p1[0], p1[1])
+        new_alfa = alfa * (i + 1)
+        p1 = make_step(p0, new_alfa, n)
+        f0 = func(p_buff)
+        f1 = func(p1)
         if verbose:
-            print(f"passo: {g(i+1)}, p0: {p0}, p1: {p1}")
+            print(f"passo: {g(i+1)}, p_buff: {p_buff}, p1: {p1}")
         if monitor is not None:
-            monitor.append(p1)
+            monitor.append(new_alfa)
         if f1 > f0:
-            return p0
-        p0 = p1
+            return p_buff
+        p_buff = p1
+
     if verbose:
         print(r("Número máximo de passos atingido."))
-    return p1
+    return p_buff
 
 
 def bissecao(
-    pi: np.ndarray,
-    pf: np.ndarray,
+    p0: np.ndarray,
     n: np.ndarray,
-    func: Callable[[float, float], float],
+    aL: float,
+    aU: float,
+    func: Callable[[np.ndarray], float],
     tol: float,
-    ε: float = 1e-8,
+    ε: float = 1e-5,
     n_max_step: int = 1000,
     verbose: bool = False,
-    monitor: list = None,
+    monitor: list[float] = None,
 ) -> np.ndarray:
     if verbose:
         print(y("Inicializando Método da Bicessão"))
-    if monitor is not None:
-        monitor.append(pi)
-
-    # norm_dir = n / np.linalg.norm(n)
-    norm_dir = n
-
-    # Realiza 1 passo para verificar se é necessário inverter a direção
-    pfpi = pf - pi
-    pfpi_norm = pfpi / np.linalg.norm(pfpi)
-    norm_n = n / np.linalg.norm(n)
-    if not np.allclose(pfpi_norm, norm_n, rtol=1e-05, atol=1e-08):
-        norm_dir = -n
 
     for i in range(n_max_step):
-        mid_point = (pi + pf) / 2
+        aM = (aL + aU) / 2
+        mid_point = make_step(p0, aM, n)
 
-        p_f1 = make_step(mid_point, -ε, norm_dir)
-        p_f2 = make_step(mid_point, ε, norm_dir)
+        p_f1 = make_step(mid_point, -ε, n)
+        p_f2 = make_step(mid_point, ε, n)
 
-        f1 = func(p_f1[0], p_f1[1])
-        f2 = func(p_f2[0], p_f2[1])
+        f1 = func(p_f1)
+        f2 = func(p_f2)
 
         if verbose:
             print(
-                f"passo: {g(i+1)}, f({mid_point[0]}, {mid_point[1]})={func(mid_point[0], mid_point[1])}"
+                f"passo: {g(i+1)}, f({mid_point[0]}, {mid_point[1]})={func(mid_point)}"
             )
         if monitor is not None:
-            monitor.append(mid_point)
-        if np.linalg.norm(pi - pf) <= tol:
+            monitor.append(aM)
+        if abs(aU - aL) <= tol:
             if verbose:
                 print(g(f"Tolerância de {tol} atingida."))
             return (p_f1 + p_f2) / 2
         if f1 > f2:
-            pi = mid_point
+            aL = aM
             continue
         if f2 > f1:
-            pf = mid_point
+            aU = aM
             continue
         raise Exception("Erro inesperado.")
+
     if verbose:
         print(r("Número máximo de passos atingido."))
     return (p_f1 + p_f2) / 2
@@ -103,7 +95,7 @@ def bissecao(
 def secao_aurea(
     pi: np.ndarray,
     pf: np.ndarray,
-    n: np.ndarray,
+    # n: np.ndarray,
     func: Callable[[float, float], float],
     tol: float,
     n_max_step: int = 1000,
@@ -115,22 +107,23 @@ def secao_aurea(
     if monitor is not None:
         monitor.append(pi)
 
+    n = pf - pi
     norm_dir = n / np.linalg.norm(n)
     ra = (np.sqrt(5) - 1) / 2
 
     # Realiza 1 passo para verificar se é necessário inverter a direção
-    beta = np.linalg.norm(pf - pi)
-    beta_dir = (1 / beta) * norm_dir
+    # beta = np.linalg.norm(pf - pi)
+    # beta_dir = (1 / beta) * norm_dir
 
-    p_f1 = make_step(pi, (1 - ra) * (1 / beta), beta_dir)
-    p_f2 = make_step(pi, ra * (1 / beta), beta_dir)
+    # p_f1 = make_step(pi, (1 - ra) * (1 / beta), beta_dir)
+    # p_f2 = make_step(pi, ra * (1 / beta), beta_dir)
 
-    mid_point = (p_f1 + p_f2) / 2
-    f1 = func(p_f1[0], p_f1[1])
-    f2 = func(p_f2[0], p_f2[1])
-    f_check = func(mid_point[0], mid_point[1])
-    if f_check > func(pi[0], pi[1]):
-        norm_dir = -n
+    # mid_point = (p_f1 + p_f2) / 2
+    # f1 = func(p_f1[0], p_f1[1])
+    # f2 = func(p_f2[0], p_f2[1])
+    # f_check = func(mid_point[0], mid_point[1])
+    # if f_check > func(pi[0], pi[1]):
+    #     norm_dir = -n
 
     for i in range(n_max_step):
         beta = np.linalg.norm(pf - pi)
--- rascunhos/lista2.py
+++ rascunhos/lista2.py
@@ -12,94 +12,104 @@ from optimization.colors import yellow as y, green as g
 
 
 alfa = 0.01
-tol = 10e-5
+tol = 1e-5
 
 
 def analise(
-    p0: np.ndarray, n: np.ndarray, f: Callable[[float, float], float], show_curves=False
+    p0: np.ndarray,
+    n: np.ndarray,
+    f: Callable[[float, float], float],
+    show_curves=False,
+    show_local=False,
 ):
-    grad_monitor_cte = []
+    # TODO: CORRIGIR FUNÇÕES DE PLOT DOS GRÁFICOS
+    road_monitor_cte = []
     cte_min = passo_constante(
         p0,
         alfa,
         n,
         f,
         verbose=False,
-        monitor=grad_monitor_cte,
+        monitor=road_monitor_cte,
     )
-    grad_points = expand(np.array(grad_monitor_cte), a)
-    grad_local = rotate_z(local(grad_points))[:, 1:3]
-    print(y("Passo Constante"))
-    # print(grad_local[0], grad_local[-1])
     print(
-        f"Mínimo encontrado: {cte_min} com valor {a(cte_min[0], cte_min[1])} com steps = {len(grad_monitor_cte)-1}"
+        y("Passo Constante")
+        + f" aU = {road_monitor_cte[-2]} aL = {road_monitor_cte[-1]}"
     )
-    # plot_local(grad_local[:, 0], grad_local[:, 1])
+    print(
+        f"Mínimo encontrado: {cte_min} com valor {f(cte_min)} com steps = {len(road_monitor_cte)}"
+    )
+    if show_local:
+        road_points = expand(np.array(road_monitor_cte), f)
+        road_local = rotate_z(local(road_points))[:, 1:3]
+        plot_local(road_local[:, 0], road_local[:, 1])
     if show_curves:
         plot_curves(p0, cte_min, f)
 
-    grad_monitor_bi = []
+    road_monitor_bi = []
     bi_min = bissecao(
         p0,
-        grad_monitor_cte[-1],
         n,
+        road_monitor_cte[0],
+        road_monitor_cte[-1],
         f,
         tol,
         verbose=False,
-        monitor=grad_monitor_bi,
+        monitor=road_monitor_bi,
     )
-    grad_points = expand(np.array(grad_monitor_bi), a)
-    grad_local = rotate_z(local(grad_points))[:, 1:3]
-    print(y("Bisseção"))
-    # print(grad_local[0], grad_local[-1])
+    print(y("Bisseção") + f" aM = {(road_monitor_bi[-2] + road_monitor_bi[-1])/2}")
     print(
-        f"Mínimo encontrado: {bi_min} com valor {a(bi_min[0], bi_min[1])} com steps = {len(grad_monitor_bi)-1}"
+        f"Mínimo encontrado: {bi_min} com valor {f(bi_min)} com steps = {len(road_monitor_bi)}"
     )
-    # plot_local(grad_local[:, 0], grad_local[:, 1])
+    if show_local:
+        road_points = expand(np.array(road_monitor_bi), f)
+        road_local = rotate_z(local(road_points))[:, 1:3]
+        plot_local(road_local[:, 0], road_local[:, 1])
     if show_curves:
         plot_curves(p0, bi_min, f)
 
-    grad_monitor_au = []
-    au_min = secao_aurea(
-        p0,
-        grad_monitor_cte[-1],
-        n,
-        f,
-        tol,
-        verbose=False,
-        monitor=grad_monitor_au,
-    )
-    grad_points = expand(np.array(grad_monitor_au), a)
-    grad_local = rotate_z(local(grad_points))[:, 1:3]
-    print(y("Seção Áurea"))
-    # print(grad_local[0], grad_local[-1])
-    print(
-        f"Mínimo encontrado: {au_min} com valor {a(au_min[0], au_min[1])} com steps = {len(grad_monitor_au)-1}"
-    )
-    # plot_local(grad_local[:, 0], grad_local[:, 1])
-    if show_curves:
-        plot_curves(p0, au_min, f)
+    # road_monitor_au = []
+    # au_min = secao_aurea(
+    #     p0,
+    #     road_monitor_cte[-1],
+    #     # n,
+    #     f,
+    #     tol,
+    #     verbose=True,
+    #     monitor=road_monitor_au,
+    # )
+    # road_points = expand(np.array(road_monitor_au), f)
+    # road_local = rotate_z(local(road_points))[:, 1:3]
+    # print(y("Seção Áurea"))
+    # # print(road_local[0], road_local[-1])
+    # print(
+    #     f"Mínimo encontrado: {au_min} com valor {f(au_min[0], au_min[1])} com steps = {len(road_monitor_au)}"
+    # )
+    # if show_local:
+    #     plot_local(road_local[:, 0], road_local[:, 1])
+    # if show_curves:
+    #     plot_curves(p0, au_min, f)
 
 
 def letra_a():
     print(g(" -> Letra A"))
     p0 = np.array([1, 2])
     n = np.array([-1, -2])
-    analise(p0, n, a, show_curves=False)
+    analise(p0, n, a, show_curves=False, show_local=False)
 
 
 def letra_b():
     print(g(" -> Letra B"))
     p0 = np.array([-2, 3])
     n = np.array([1.453, -4.547])
-    analise(p0, n, mc_cormick, show_curves=False)
+    analise(p0, n, mc_cormick, show_curves=False, show_local=False)
 
 
 def letra_c():
     print(g(" -> Letra C"))
     p0 = np.array([0, 5])
     n = np.array([3, 1.5])
-    analise(p0, n, himmelblau, show_curves=False)
+    analise(p0, n, himmelblau, show_curves=False, show_local=False)
 
 
 if __name__ == "__main__":
